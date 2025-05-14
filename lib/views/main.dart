1e76e454058ef36b815514c0dbb6d112f34518cc
Youssef Amr <youssefamr2244@gmail.com>
2025-05-14 05:03:24 +0300

    -

--- lib/views/main.dart
+++ lib/views/main.dart
@@ -1,21 +1,11 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import '../controller/home_provider.dart';
 import 'onboarding.dart';
 
 void main() {
   runApp(
-    DevicePreview(
-      enabled: true,
-      builder: (context) => MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (context) => HomeProvider())
-        ],
-        child: const MyApp(),
-      ),
-    ),
+    const MyApp(),
   );
 }
 
@@ -25,18 +15,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      useInheritedMediaQuery: true,
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
-      darkTheme: ThemeData.dark(),
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black38),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => HomeProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.black38),
+          useMaterial3: true,
+        ),
+        home: const onborading(),
       ),
-      home: onborading(),
     );
   }
 }
